d520fccd73ae3807d2092ad77e72bb5e996b8f7b
Raquel-Santos <80425930+Raquel-Santos@users.noreply.github.com>
2023-11-05 16:42:19 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,16 +5,32 @@ from Ceil import Ceil
 from Floor import Floor
 from Krappy import Krappy
 from MaskFish import MaskFish
+from tubarao import Tubarao
 from Score import Score
 
-background = Background()
-ceil1 = Ceil(0)
-ceil2 = Ceil(1080)
-floor1 = Floor(0)
-floor2 = Floor(1080)
-score = Score()
-bubblefish = BubbleFish(200,200)
-krappy = Krappy(400,100)
-maskfish = MaskFish(400)
+class Play(Image):
+    
+    def update(self) -> None:
+    
+        
+        if keyboard.is_key_just_down('space'):
+            
+            background = Background()
+            ceil1 = Ceil(0)
+            ceil2 = Ceil(1080)
+            floor1 = Floor(0)
+            floor2 = Floor(1080)    
+            score = Score()        
+            bubblefish = BubbleFish(200,200)
+            krappy = Krappy(400,100)
+            tubarao = Tubarao(940,250)
+            maskfish = MaskFish(400)
+            
 
+
+play = Play ('play.png')
+
+
+play.x = 500
+play.y = 250
 run(globals())
